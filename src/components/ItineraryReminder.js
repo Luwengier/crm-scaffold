44d0e141d2d8ece304264fae52a8c93fa7c3e775
Luwengier <g217881111@gmail.com>
2021-12-07 19:21:39 +0800

    Add ContentSkeleton component

--- src/components/ItineraryReminder.js
+++ src/components/ItineraryReminder.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Fab, Badge, Popover, Paper, Typography, Button, Card, CardActions, CardContent, CardMedia } from '@mui/material'
+import { Fab, Badge, Popover, Typography, Button, Card, CardActions, CardContent, CardMedia } from '@mui/material'
 import TextSnippetIcon from '@mui/icons-material/TextSnippet'
 
 export default function ItineraryReminder() {
@@ -17,64 +17,77 @@ export default function ItineraryReminder() {
 
   return (
     <React.Fragment>
-      <Button variant="contained" onClick={handleClick}>123</Button>
       <Popover
         open={open}
         onClose={handleClose}
-        anchorReference="anchorPosition"
-        anchorPosition={{ top: 0, left: 0 }}
-      >
-        <Paper sx={{
+        // anchorReference="anchorPosition"
+        // anchorPosition={{ top: 0, left: 0 }}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        marginThreshold={0}
+        PaperProps={{
+          sx: {
+            height: '100%',
+            maxHeight: '100%',
+            padding: '1rem',
+          },
+        }}
+        sx={{
           zIndex: 1600,
           width: 360,
-          height: '85vh',
-        }}>
+        }}
+      >
+        <Card sx={{ maxWidth: 345, mb: '0.75rem', }}>
+          <CardMedia
+            component="img"
+            height="140"
+            image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Lizard
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over 6,000
+              species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component="img"
+            height="140"
+            image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Lizard
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over 6,000
+              species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
 
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              component="img"
-              height="140"
-              image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              component="img"
-              height="140"
-              image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-
-        </Paper>
       </Popover>
+
       <Fab
         onClick={handleClick}
         sx={{
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,25 @@
 import React from 'react'
-import { Box, Typography } from '@mui/material'
+import { Box, Grid, Skeleton } from '@mui/material'
 import MiniDrawer from '../components/MiniDrawer'
 import ItineraryReminder from '../components/ItineraryReminder'
+
+function ContentSkeleton(props) {
+  const { displayNumber = 3 } = props
+  return (
+    <Grid container>
+      {Array.from(new Array(displayNumber)).map((item, index) => (
+        <Grid item xl={2} md={3} xs={6} key={index} sx={{ my: 4, px: 1 }}>
+          <Skeleton variant="rectangular" animation="wave" height={118} />
+
+          <Box sx={{ pt: 0.5 }}>
+            <Skeleton />
+            <Skeleton width="60%" />
+          </Box>
+        </Grid>
+      ))}
+    </Grid>
+  )
+}
 
 export default function Home() {
 
@@ -10,33 +28,7 @@ export default function Home() {
       <MiniDrawer />
       <ItineraryReminder />
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-          Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-          nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-          leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-          feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-          sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+        <ContentSkeleton displayNumber={18} />
       </Box>
     </Box>
   )
